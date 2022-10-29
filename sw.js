0f9e384b59304c522a3cdbe225e99634b07eaab2
Michel Orozco Carrera <michel.orozco@utcv.edu.mx>
2022-10-29 11:35:55 -0500

    se actualiza el service worker

--- sw.js
+++ sw.js
@@ -1,9 +1,16 @@
 self.addEventListener('install', (event) => {
-    console.info('[SW] Install ...');
+    // Nosotros deberíamos agregar al cache nuestros archivos
+    console.info('[SW]: Adicionando nuestros archivos al cache...');
+
+    setTimeout(() = {
+        console.warn('[SW]: Se instaló correctamente...');
+    }, 1000); // 1 segundo
+    self.skipWaiting();
 });
 self.addEventListener('activate', async (event) => {
-    console.info('[SW] Activate ...');
+    console.info('[SW]: Archivos exitosamente guardados...');
+    //event.eaitUntil(clients.claim());
 });
 self.addEventListener('fetch', (event) => {
-    console.info('[SW] Fetch ...');
+    console.info('[SW]: Instalando');
 });
