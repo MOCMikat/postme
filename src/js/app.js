01637ca485840e40e7c43f0e6adb77716019330d
Michel Orozco Carrera <michel.orozco@utcv.edu.mx>
2022-10-29 09:33:04 -0500

    actualización del index

--- src/js/app.js
+++ src/js/app.js
@@ -19,6 +19,7 @@ const closePostModal = () => {
 };
 
 window.addEventListener('beforeinstallprompt', (e) =>  {
+    console.log('boton instalar...');
     e.preventDefault();
     deferredPrompt = e;
 });
